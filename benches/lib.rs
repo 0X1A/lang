83daa5abece8e0b62e36b3d4e66d80e0689c13df
Alberto Corona <albcoron@gmail.com>
2019-05-09 07:56:39 -0500

    Add generated tests and benches

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,17 +1,247 @@
+// This file is auto-generated. Please do not edit it manually.
+
 #[macro_use]
 extern crate criterion;
 extern crate lang;
 
-use criterion::black_box;
-use criterion::Criterion;
-
 use self::lang::lang::Lang;
+use criterion::{black_box, Criterion};
 
-fn variable_empty_decl(c: &mut Criterion) {
-    c.bench_function("variable empty declaration", |b| {
-        b.iter(|| Lang::new(black_box(Some("let variable: i32;"))))
+fn array_i64_variable_declaration(c: &mut Criterion) {
+    c.bench_function("Array<i64> Variable declaration", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: Array<i64>;"))))
     });
 }
+fn array_i64_variable_declaration_and_assignment(c: &mut Criterion) {
+    c.bench_function("Array<i64> Variable declaration and assignment", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: Array<i64> = [0, 1, 2];"))))
+    });
+}
+fn array_i64_variable_declaration_empty(c: &mut Criterion) {
+    c.bench_function("Array<i64> Variable declaration empty", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: Array<i64> = [];"))))
+    });
+}
+fn array_i64_variable_re_assignment(c: &mut Criterion) {
+    c.bench_function("Array<i64> Variable re-assignment", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "let i: Array<i64> = [];
+            i = [0, 1, 2];",
+            )))
+        })
+    });
+}
+fn array_i64_variable_re_assignment_failure(c: &mut Criterion) {
+    c.bench_function("Array<i64> Variable re-assignment failure", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "let i: Array<i64> = [];
+            i = [0.00, 1.00, 2.00];",
+            )))
+        })
+    });
+}
+fn struct_declaration(c: &mut Criterion) {
+    c.bench_function("Struct declaration", |b| {
+        b.iter(|| Lang::new(black_box(Some("struct TestStruct {}"))))
+    });
+}
+fn struct_declaration_failure(c: &mut Criterion) {
+    c.bench_function("Struct declaration failure", |b| {
+        b.iter(|| Lang::new(black_box(Some("struct TestStruct {{}"))))
+    });
+}
+fn struct_declaration_with_fields(c: &mut Criterion) {
+    c.bench_function("Struct declaration with fields", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "
+        struct TestStruct {
+            field0: i64,
+            field1: f64,
+            field2: bool
+        }
+        ",
+            )))
+        })
+    });
+}
+fn struct_with_field_access_failure(c: &mut Criterion) {
+    c.bench_function("Struct with field access failure", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "
+        struct TestStruct {}
 
-criterion_group!(benches, variable_empty_decl);
-criterion_main!(benches);
+        let instance: TestStruct = TestStruct();
+        instance.field_doesnt_exist;
+        ",
+            )))
+        })
+    });
+}
+fn struct_with_fields_instatiation(c: &mut Criterion) {
+    c.bench_function("Struct with fields instatiation", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "
+        struct TestStruct {
+            field0: i64,
+            field1: f64,
+            field2: bool
+        }
+
+        let instance: TestStruct = TestStruct();
+        ",
+            )))
+        })
+    });
+}
+fn struct_with_fields_instatiation_and_field_assignment(c: &mut Criterion) {
+    c.bench_function(
+        "Struct with fields instatiation and field assignment",
+        |b| {
+            b.iter(|| {
+                Lang::new(black_box(Some(
+                    "
+        struct TestStruct {
+            field0: i64,
+            field1: f64,
+            field2: bool
+        }
+
+        let instance: TestStruct = TestStruct();
+        instance.field0 = 0;
+        instance.field1 = 1.00;
+        instance.field2 = false;
+        ",
+                )))
+            })
+        },
+    );
+}
+fn struct_with_impl(c: &mut Criterion) {
+    c.bench_function("Struct with impl", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "
+        struct TestStruct {
+        }
+
+        impl TestStruct {
+            fn hello() -> () {
+                print \"Hello world\";
+            }
+        }
+
+        let instance: TestStruct = TestStruct();
+        instance.hello();
+        ",
+            )))
+        })
+    });
+}
+fn struct_with_method_call_failure(c: &mut Criterion) {
+    c.bench_function("Struct with method call failure", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "
+        struct TestStruct {
+        }
+
+        impl TestStruct {
+        }
+
+        let instance: TestStruct = TestStruct();
+        instance.hello();
+        ",
+            )))
+        })
+    });
+}
+fn f64_variable_declaration(c: &mut Criterion) {
+    c.bench_function("f64 Variable declaration", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: f64;"))))
+    });
+}
+fn f64_variable_declaration_and_assignment(c: &mut Criterion) {
+    c.bench_function("f64 Variable declaration and assignment", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: f64 = 0.00;"))))
+    });
+}
+fn f64_variable_re_assignment(c: &mut Criterion) {
+    c.bench_function("f64 Variable re-assignment", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "let i: f64 = 0.00;
+            i = 100.00;",
+            )))
+        })
+    });
+}
+fn f64_variable_re_assignment_failure(c: &mut Criterion) {
+    c.bench_function("f64 Variable re-assignment failure", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "let i: f64 = 0;
+        i = 100;",
+            )))
+        })
+    });
+}
+fn i64_variable_declaration(c: &mut Criterion) {
+    c.bench_function("i64 Variable declaration", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: i64;"))))
+    });
+}
+fn i64_variable_declaration_and_assignment(c: &mut Criterion) {
+    c.bench_function("i64 Variable declaration and assignment", |b| {
+        b.iter(|| Lang::new(black_box(Some("let i: i64 = 0;"))))
+    });
+}
+fn i64_variable_re_assignment(c: &mut Criterion) {
+    c.bench_function("i64 Variable re-assignment", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "let i: i64 = 0;
+            i = 100;",
+            )))
+        })
+    });
+}
+fn i64_variable_re_assignment_failure(c: &mut Criterion) {
+    c.bench_function("i64 Variable re-assignment failure", |b| {
+        b.iter(|| {
+            Lang::new(black_box(Some(
+                "let i: i64 = 0;
+        i = 100;",
+            )))
+        })
+    });
+}
+criterion_group!(
+    lang_benches,
+    array_i64_variable_declaration,
+    array_i64_variable_declaration_and_assignment,
+    array_i64_variable_declaration_empty,
+    array_i64_variable_re_assignment,
+    array_i64_variable_re_assignment_failure,
+    struct_declaration,
+    struct_declaration_failure,
+    struct_declaration_with_fields,
+    struct_with_field_access_failure,
+    struct_with_fields_instatiation,
+    struct_with_fields_instatiation_and_field_assignment,
+    struct_with_impl,
+    struct_with_method_call_failure,
+    f64_variable_declaration,
+    f64_variable_declaration_and_assignment,
+    f64_variable_re_assignment,
+    f64_variable_re_assignment_failure,
+    i64_variable_declaration,
+    i64_variable_declaration_and_assignment,
+    i64_variable_re_assignment,
+    i64_variable_re_assignment_failure
+);
+criterion_main!(lang_benches);
--- tests/test.rs
+++ tests/test.rs
@@ -1,88 +1,278 @@
+// This file is auto-generated. Please do not edit it manually.
+
 #[cfg(test)]
 mod tests {
-
     extern crate lang;
-
     use self::lang::lang::Lang;
-    // Test variable declaration
-    #[test]
-    fn variable_empty_decl() {
-        let mut lang = Lang::new(Some("let variable: i32;"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, true);
-    }
 
-    // Test variable declaration
     #[test]
-    fn variable_decl() {
-        let mut lang = Lang::new(Some("let variable: i64 = 100; print variable;"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, true);
+    fn array_i64_variable_declaration() {
+        let mut lang = Lang::new(Some("let i: Array<i64>;"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
     }
-
-    // Test variable declaration
     #[test]
-    fn variable_i32_type_assignment_failure() {
-        let mut lang = Lang::new(Some("let variable: i32 = false;"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, false);
+    fn array_i64_variable_declaration_and_assignment() {
+        let mut lang = Lang::new(Some("let i: Array<i64> = [0, 1, 2];"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
     }
-
-    // Test variable declaration
     #[test]
-    fn variable_f32_type_assignment_failure() {
-        let mut lang = Lang::new(Some("let variable: f32 = \"Hello World\";"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, false);
+    fn array_i64_variable_declaration_empty() {
+        let mut lang = Lang::new(Some("let i: Array<i64> = [];"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
     }
-
-    // Test variable declaration
     #[test]
-    fn variable_string_type_assignment_failure() {
-        let mut lang = Lang::new(Some("let variable: String = 100;"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, false);
-    }
-
-    // Test variable declaration
-    #[test]
-    fn struct_decl() {
-        let mut lang = Lang::new(Some("struct TestStruct {}"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, true);
-    }
-
-    // Test variable declaration
-    #[test]
-    fn struct_decl_with_fields() {
-        let mut lang = Lang::new(Some("struct TestStruct { one: i32, two: String }"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, true);
-    }
-
-    // Test variable declaration
-    #[test]
-    fn struct_decl_with_fields_failure() {
-        let mut lang = Lang::new(Some("struct TestStruct { one: two: String }"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, false);
-    }
-
-    // Test variable declaration
-    #[test]
-    fn struct_decl_with_impl() {
-        let mut lang = Lang::new(Some("struct TestStruct { one: i32 } impl TestStruct { }"));
-        let result = lang.run().is_ok();
-        assert_eq!(result, true);
-    }
-
-    // Test variable declaration
-    #[test]
-    fn array_index() {
+    fn array_i64_variable_re_assignment() {
         let mut lang = Lang::new(Some(
-            "let arr: Array<i64> = [0, 1, 2]; let idx: i64 = arr[0];",
+            "let i: Array<i64> = [];
+            i = [0, 1, 2];",
         ));
-        let result = lang.run().is_ok();
-        assert_eq!(result, true);
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn array_i64_variable_re_assignment_failure() {
+        let mut lang = Lang::new(Some(
+            "let i: Array<i64> = [];
+            i = [0.00, 1.00, 2.00];",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), false)
+    }
+    #[test]
+    fn struct_declaration() {
+        let mut lang = Lang::new(Some("struct TestStruct {}"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn struct_declaration_failure() {
+        let mut lang = Lang::new(Some("struct TestStruct {{}"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), false)
+    }
+    #[test]
+    fn struct_declaration_with_fields() {
+        let mut lang = Lang::new(Some(
+            "
+        struct TestStruct {
+            field0: i64,
+            field1: f64,
+            field2: bool
+        }
+        ",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn struct_with_field_access_failure() {
+        let mut lang = Lang::new(Some(
+            "
+        struct TestStruct {}
+
+        let instance: TestStruct = TestStruct();
+        instance.field_doesnt_exist;
+        ",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), false)
+    }
+    #[test]
+    fn struct_with_fields_instatiation() {
+        let mut lang = Lang::new(Some(
+            "
+        struct TestStruct {
+            field0: i64,
+            field1: f64,
+            field2: bool
+        }
+
+        let instance: TestStruct = TestStruct();
+        ",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn struct_with_fields_instatiation_and_field_assignment() {
+        let mut lang = Lang::new(Some(
+            "
+        struct TestStruct {
+            field0: i64,
+            field1: f64,
+            field2: bool
+        }
+
+        let instance: TestStruct = TestStruct();
+        instance.field0 = 0;
+        instance.field1 = 1.00;
+        instance.field2 = false;
+        ",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn struct_with_impl() {
+        let mut lang = Lang::new(Some(
+            "
+        struct TestStruct {
+        }
+
+        impl TestStruct {
+            fn hello() -> () {
+                print \"Hello world\";
+            }
+        }
+
+        let instance: TestStruct = TestStruct();
+        instance.hello();
+        ",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn struct_with_method_call_failure() {
+        let mut lang = Lang::new(Some(
+            "
+        struct TestStruct {
+        }
+
+        impl TestStruct {
+        }
+
+        let instance: TestStruct = TestStruct();
+        instance.hello();
+        ",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), false)
+    }
+    #[test]
+    fn f64_variable_declaration() {
+        let mut lang = Lang::new(Some("let i: f64;"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn f64_variable_declaration_and_assignment() {
+        let mut lang = Lang::new(Some("let i: f64 = 0.00;"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn f64_variable_re_assignment() {
+        let mut lang = Lang::new(Some(
+            "let i: f64 = 0.00;
+            i = 100.00;",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn f64_variable_re_assignment_failure() {
+        let mut lang = Lang::new(Some(
+            "let i: f64 = 0;
+        i = 100;",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), false)
+    }
+    #[test]
+    fn i64_variable_declaration() {
+        let mut lang = Lang::new(Some("let i: i64;"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn i64_variable_declaration_and_assignment() {
+        let mut lang = Lang::new(Some("let i: i64 = 0;"));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn i64_variable_re_assignment() {
+        let mut lang = Lang::new(Some(
+            "let i: i64 = 0;
+            i = 100;",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), true)
+    }
+    #[test]
+    fn i64_variable_re_assignment_failure() {
+        let mut lang = Lang::new(Some(
+            "let i: i64 = 0;
+        i = 100;",
+        ));
+        let result = lang.run();
+        if let Err(ref error) = result {
+            println!("{}", error);
+        }
+        assert_eq!(result.is_ok(), false)
     }
 }
